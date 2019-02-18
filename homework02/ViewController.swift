d79d3a76c566f00b8765c443204ed4e34baac9de
Maria Handschu <Maripau@macbook-air.local>
2019-02-18 10:36:34 -0500

    Initial Commit

---
+++ homework02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework02
+//
+//  Created by Maria Handschu on 2/18/19.
+//  Copyright © 2019 Maria Handschu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
