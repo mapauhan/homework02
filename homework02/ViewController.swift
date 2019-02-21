84f895042cb4dcb921d410fb5dc387456d9f0e29
Maria Handschu <Maripau@macbook-air.local>
2019-02-21 12:46:09 -0500

    Contact VC and Details VC

--- homework02/ViewController.swift
+++ homework02/ViewController.swift
@@ -8,13 +8,86 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var contactList:  [Contact] = []//saves an empty array
+    var newContact = Contact()//saves contact info from newContactVC
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print( "viewDidLoad" )
+
+    }
+    
+   override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        print("viewDidAppear")
+        tableView.reloadData()
     }
 
-
+    @IBAction func deleteContact(_ sender: Any) {
+    
+    }
+    
+    
+    @IBAction func goBack(unwindSegue: UIStoryboardSegue) {
+        
+        if unwindSegue.identifier == "addContactSegue" {
+            contactList.append(newContact)
+        }
+    }
 }
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contactList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath)
+        let contact = contactList[indexPath.row]
+        
+        let name = cell.viewWithTag(102) as! UILabel
+        let email = cell.viewWithTag(103) as! UILabel
+        let phone = cell.viewWithTag(104) as! UILabel
+        let phoneType = cell.viewWithTag(105) as! UILabel
+        
+        name.text = contact.name
+        email.text = contact.email
+        phone.text = contact.phoneNum
+       phoneType.text = String(contact.phoneType!)
+       
+        if contact.phoneType == 0 {
+            phoneType.text = "Cell"
+        }
+        else if contact.phoneType == 1 {
+            phoneType.text = "Home"
+        } else {
+            phoneType.text = "Office"
+        }
+
+        print(contact)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        newContact = contactList[indexPath.row]
+//      let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath)
+        print("Selected row\(newContact)")
+        performSegue(withIdentifier: "DetailsSeg", sender: nil)
+
+    
+    
+    
+    }
+}
+    
+
+
+    
+    
 
